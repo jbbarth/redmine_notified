812764a3de8bfa23e219e3555bfc6d627316d908
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-30 01:19:35 +0200

    Deface should only be used for html views, not API

--- app/overrides/issues/form.rb
+++ app/overrides/issues/form.rb
@@ -1,9 +1,9 @@
-Deface::Override.new :virtual_path  => 'issues/new',
+Deface::Override.new :virtual_path  => 'issues/new.html',
                      :name          => 'add-notified-users-in-issues-new',
                      :insert_bottom => 'div.tabular',
                      :partial       => 'issues/notified_users_form'
 
-Deface::Override.new :virtual_path  => 'issues/_edit',
+Deface::Override.new :virtual_path  => 'issues/_edit.html',
                      :name          => 'add-notified-users-in-issues-edit',
                      :insert_bottom => 'fieldset:contains("label_change_properties")',
                      :partial       => 'issues/notified_users_form'
