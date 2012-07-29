0ab113d88b225e8698732720d3ca4152a87a1371
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-30 01:45:08 +0200

    Revert "Deface should only be used for html views, not API"
    This reverts commit 812764a3de8bfa23e219e3555bfc6d627316d908.

--- app/overrides/issues/form.rb
+++ app/overrides/issues/form.rb
@@ -1,9 +1,9 @@
-Deface::Override.new :virtual_path  => 'issues/new.html',
+Deface::Override.new :virtual_path  => 'issues/new',
                      :name          => 'add-notified-users-in-issues-new',
                      :insert_bottom => 'div.tabular',
                      :partial       => 'issues/notified_users_form'
 
-Deface::Override.new :virtual_path  => 'issues/_edit.html',
+Deface::Override.new :virtual_path  => 'issues/_edit',
                      :name          => 'add-notified-users-in-issues-edit',
                      :insert_bottom => 'fieldset:contains("label_change_properties")',
                      :partial       => 'issues/notified_users_form'
