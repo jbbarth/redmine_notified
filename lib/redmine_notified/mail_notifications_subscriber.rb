3b444eca3d80aa01c02883ce759c6927a5112061
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-02 07:00:45 +0200

    Typo in previous commit

--- lib/redmine_notified/mail_notifications_subscriber.rb
+++ lib/redmine_notified/mail_notifications_subscriber.rb
@@ -7,6 +7,6 @@ ActiveSupport::Notifications.subscribe "deliver.action_mailer" do |name, start, 
     Notification.create(attrs.slice(*Notification.column_names))
   rescue
     #this shouldn't happen, but hey, we don't want to prevent ticket creation if anything fails
-    Rails.logger.error("ERROR: redmine_notified plugin, unable to add notification for #{attrs.inspect}"
+    Rails.logger.error("ERROR: redmine_notified plugin, unable to add notification for #{attrs.inspect}")
   end
 end
