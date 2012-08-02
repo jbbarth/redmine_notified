eb6a3b15d84f76d86be701430b7fcc94bd2fda57
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-02 06:47:41 +0200

    Ensure we don't prevent ticket creation if notification creation fails

--- lib/redmine_notified/mail_notifications_subscriber.rb
+++ lib/redmine_notified/mail_notifications_subscriber.rb
@@ -3,5 +3,10 @@ ActiveSupport::Notifications.subscribe "deliver.action_mailer" do |name, start, 
   %w(from to cc bcc).each do |key|
     attrs[key] = attrs[key].join(", ") if attrs.has_key?(key)
   end
-  Notification.create(attrs.slice(*Notification.column_names))
+  begin
+    Notification.create(attrs.slice(*Notification.column_names))
+  rescue
+    #this shouldn't happen, but hey, we don't want to prevent ticket creation if anything fails
+    Rails.logger.error("ERROR: redmine_notified plugin, unable to add notification for #{attrs.inspect}"
+  end
 end
