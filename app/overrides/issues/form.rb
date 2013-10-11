e9ad6a443d378ccaaae15de26c9072ce2d531a5c
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-11 09:07:25 +0200

    Move notified users information at the exact same place in forms

--- app/overrides/issues/form.rb
+++ app/overrides/issues/form.rb
@@ -1,11 +1,11 @@
 Deface::Override.new :virtual_path  => 'issues/new',
                      :original      => '2f69a4138473a22e544a1fe043d9874aae820445',
                      :name          => 'add-notified-users-in-issues-new',
-                     :insert_bottom => 'div.tabular',
+                     :insert_after  => '#attachments_form',
                      :partial       => 'issues/notified_users_form'
 
 Deface::Override.new :virtual_path  => 'issues/_edit',
                      :original      => '15c25198ca7b9049abbd5f1c32c2d4c6da4c285e',
                      :name          => 'add-notified-users-in-issues-edit',
-                     :insert_bottom => 'fieldset:contains("label_change_properties")',
+                     :insert_bottom => 'fieldset:contains("field_notes")',
                      :partial       => 'issues/notified_users_form'
