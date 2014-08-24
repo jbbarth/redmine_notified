d00b809fce41ad36ff1c0080e90a0f1d75db773c
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-24 15:35:55 +0200

    Depend on redmine_base_deface and remove dirty hacks for deface integration

--- init.rb
+++ init.rb
@@ -2,14 +2,6 @@ require 'redmine'
 require 'redmine_notified/hooks'
 require 'redmine_notified/mail_notifications_subscriber'
 
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 ActionDispatch::Callbacks.to_prepare do
   require_dependency 'redmine_notified/notifications_association_patch'
 end
@@ -23,6 +15,7 @@ Redmine::Plugin.register :redmine_notified do
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_notified'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   project_module :issue_tracking do
     permission :view_notified_users, { :notified => [:show] }
   end
