2088ca2ecb70d4f706c3468da9cc5a8e7d02f1c0
Vincent Robert <vincent.robert@nanego.com>
2020-03-28 14:01:53 +0100

    Minor UI update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,6 @@
 # Load the normal Rails helper
 require File.expand_path("../../../redmine_base_rspec/spec/spec_helper", __FILE__)
 
-# Taken from core
-def with_settings(options, &block)
-  saved_settings = options.keys.inject({}) do |h, k|
-    h[k] = case Setting[k]
-      when Symbol, false, true, nil
-        Setting[k]
-      else
-        Setting[k].dup
-      end
-    h
-  end
-  options.each {|k, v| Setting[k] = v}
-  yield
-ensure
-  saved_settings.each {|k, v| Setting[k] = v} if saved_settings
-end
-
 def assert_mail_body_match(expected, mail, message=nil)
   if expected.is_a?(String)
     expect(mail_body(mail)).to include(expected)
