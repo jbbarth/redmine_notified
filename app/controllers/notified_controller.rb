4aa76ab65be7962adf4e0c0a1bae253038e698dd
Vincent Robert <vincent.robert@nanego.com>
2021-04-22 15:14:16 +0200

    Display all saved notifications including create events

--- app/controllers/notified_controller.rb
+++ app/controllers/notified_controller.rb
@@ -4,7 +4,7 @@ class NotifiedController < ApplicationController
   before_action :authorize
 
   def show
-    @issue_notification = Notification.where("notificable_type = 'Issue' AND notificable_id = ?", @issue.id).first
+    @issue_notifications = Notification.where("notificable_type = 'Issue' AND notificable_id = ?", @issue.id)
     @journal_notifications = Notification.where("notificable_type = 'Journal' AND notificable_id IN (?)", @issue.journal_ids)
                                           .order("created_at asc")
     @journal_ids = @issue.journals.order("created_on asc").pluck(:id)
