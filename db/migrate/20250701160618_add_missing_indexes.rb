f0559f70e704a67465b2f5be0d9029ca331b9988
Vincent <contact@vincent-robert.com>
2025-09-25 08:12:27 +0200

    Remove obsolete CI workflow files

--- db/migrate/20250701160618_add_missing_indexes.rb
+++ db/migrate/20250701160618_add_missing_indexes.rb
@@ -1,4 +1,4 @@
-class AddMissingIndexes < ActiveRecord::Migration[7.2]
+class AddMissingIndexes < ActiveRecord::Migration[6.1]
   def change
     add_index :notifications, [:notificable_id, :notificable_type], if_not_exists: true
   end
