a1b2cdafcbd1899da4ca510185687dd46b65af1e
Vincent <contact@vincent-robert.com>
2026-08-18 19:50:19 +0200

    Use core SVG sprite icons instead of legacy raster icons

--- app/overrides/issues/form.rb
+++ app/overrides/issues/form.rb
@@ -1,11 +1,9 @@
 Deface::Override.new :virtual_path  => 'issues/new',
-                     :original      => '2f69a4138473a22e544a1fe043d9874aae820445',
                      :name          => 'add-notified-users-in-issues-new',
                      :insert_after  => '#attachments_form',
                      :partial       => 'issues/notified_users_form'
 
 Deface::Override.new :virtual_path  => 'issues/_edit',
-                     :original      => '15c25198ca7b9049abbd5f1c32c2d4c6da4c285e',
                      :name          => 'add-notified-users-in-issues-edit',
                      :insert_bottom => 'fieldset:contains("field_notes")',
                      :partial       => 'issues/notified_users_form'
